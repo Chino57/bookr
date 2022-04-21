add52234bddfe005801d7b2b9181ff00c4808b2f
Julien Pernin <pernin.julien@gmail.com>
2022-04-21 06:55:15 +0200

    add forms
    added forms for add Publisher and Reviews

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from .models import Publisher, Review
 
 RADIO_CHOICES = (("Value One", "Value One Display"),
                  ("Value Two", "Text For Value Two"),
@@ -31,3 +33,51 @@ class SearchForm(forms.Form):
     search = forms.CharField(required=False, min_length=3)
     search_in = forms.ChoiceField(required=False, choices=(("title", "Title"),
                                                            ("contributor", "Contributor")))
+
+
+class NewsletterSignupForm(forms.Form):
+    signup = forms.BooleanField(label="Sign up to newsletter?", required=False)
+    email = forms.EmailField(help_text="Enter your email address to subscribe", required=False)
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if cleaned_data['signup'] and not cleaned_data.get("email"):
+            self.add_error("email", "Your email address is required if signing up for the newsletter.")
+
+
+class OrderForm(forms.Form):
+    def validate_email_domain(value):
+        if value.split("@")[-1].lower() != "example.com":
+            raise ValidationError("The email address must be on the domain example.com.")
+
+    def clean_email(self):
+        return self.cleaned_data["email"].lower()
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data["send_confirmation"] and not cleaned_data.get("email"):
+            self.add_error("email", "Please enter an email address to receive the confirmation message.")
+        elif cleaned_data.get("email") and not cleaned_data["send_confirmation"]:
+            self.add_error("send_confirmation", "Please check this if you want to receive a confirmation email")
+        item_total = cleaned_data.get("magazine_count", 0) + cleaned_data.get("book_count", 0)
+        if item_total > 100:
+            self.add_error(None, "The total number of items must be 100 or less.")
+
+    magazine_count = forms.IntegerField(min_value=0, max_value=80, widget=forms.NumberInput(attrs={"placeholder" : "Number of Magazines"}))
+    book_count = forms.IntegerField(min_value=0, max_value=50, widget=forms.NumberInput(attrs={"placeholder": "Number of Books"}))
+    send_confirmation = forms.BooleanField(required=False)
+    email = forms.EmailField(required=False, validators=[validate_email_domain], widget=forms.EmailInput(attrs={"placeholder": "Your company email address"}))
+
+
+class PublisherForm(forms.ModelForm):
+    class Meta:
+        model = Publisher
+        fields = "__all__"
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        exclude = ("date_edited", "book")
+
+    rating = forms.IntegerField(min_value=0, max_value=5)
--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
+from django.contrib import messages
+from django.utils.timezone import now
 
-from .models import Book, Review, Contributor
+from .models import Book, Review, Contributor, Publisher
 from .utils import average_rating
 
-from .forms import SearchForm, ExampleForm
+from .forms import SearchForm, PublisherForm, ReviewForm
 
 
 def index(request):
@@ -59,3 +61,67 @@ def book_detail(request,pk):
         context = {"book": book, "book_rating": None, "reviews": None }
 
     return render(request, 'reviews/book_details.html', context)
+
+
+def form_example(request):
+    initial = {"email": "user@example.com"}
+    if request.method == "POST":
+        form = OrderForm(request.POST, initial=initial)
+    else:
+        form = OrderForm(initial=initial)
+
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            for name, value in form.cleaned_data.items():
+                print("{}: ({}) {}".format(name, type(value), value))
+
+    return render(request, "reviews/form-example.html", {"method": request.method, "form": form})
+
+
+def publisher_edit(request, pk=None):
+    if pk is not None:
+        publisher = get_object_or_404(Publisher, pk=pk)
+    else:
+        publisher = None
+    if request.method == "POST":
+        form = PublisherForm(request.POST, instance=publisher)
+        if form.is_valid():
+            updated_publisher = form.save()
+            if publisher is None:
+                messages.success(request, "Publisher \"{}\" was created.".format(updated_publisher))
+            else:
+                messages.success(request, "Publisher \"{}\" was updated.".format(updated_publisher))
+            return redirect("publisher_edit", updated_publisher.pk)
+    else:
+        form = PublisherForm(instance=publisher)
+        return render(request, "reviews/instance-form.html", {"instance": publisher,"model_type": "Publisher", "form": form})
+
+
+def review_edit(request, book_pk, review_pk=None):
+    book = get_object_or_404(Book, pk=book_pk)
+    if review_pk is not None:
+        review = get_object_or_404(Review, book_id=book_pk, pk=review_pk)
+    else:
+        review = None
+
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+
+        if form.is_valid():
+            updated_review = form.save(False)
+            updated_review.book = book
+            if review is None:
+                messages.success(request,"Review for \"{}\" was created".format(book))
+            else:
+                updated_review.date_edited = now()
+                updated_review.save()
+                messages.success(request, "Review for \"{}\" was updated".format(book))
+            return redirect("book_detail", book_pk)
+
+    else:
+        form = ReviewForm(instance=review)
+
+    return render(request, "reviews/instance-form.html", {"instance": review, "model_type": "Review", "form": form,
+                                                          "related_model_type": Book, "related_instance": book})
+
