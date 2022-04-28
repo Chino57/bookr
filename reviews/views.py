8ec19b3a5796f214bcc683217e088c626600a0d7
Julien Pernin <pernin.julien@gmail.com>
2022-04-28 09:11:43 +0200

    add authentication and session
    added profile, authentication system and session

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,get_object_or_404, redirect
 from django.contrib import messages
 from django.utils.timezone import now
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.core.exceptions import PermissionDenied
 
 from .models import Book, Review, Contributor, Publisher
 from .utils import average_rating
@@ -11,14 +13,20 @@ from PIL import Image
 from django.core.files.images import ImageFile
 
 
+def is_staff_user(user):
+    return user.is_staff
+
+
 def index(request):
     return render(request, "base.html")
 
 
 def book_search(request):
     search_text = request.GET.get("search", "")
+    search_history = request.session.get('search_history', [])
     form = SearchForm(request.GET)
     books = set()
+
     if form.is_valid() and form.cleaned_data["search"]:
         search = form.cleaned_data["search"]
         search_in = form.cleaned_data.get("search_in") or "title"
@@ -29,10 +37,16 @@ def book_search(request):
             for contributor in fname_contributors:
                 for book in contributor.book_set.all():
                     books.add(book)
-            lname_contributors = Contributor.objects.filter(last_names__icontains=search)
-            for contributor in lname_contributors:
-                for book in contributor.book_set.all():
-                    book.add(book)
+        lname_contributors = Contributor.objects.filter(last_names__icontains=search)
+        for contributor in lname_contributors:
+            for book in contributor.book_set.all():
+                book.add(book)
+        if request.user.is_authenticated:
+            search_history.append([search_in, search])
+            request.session['search_history'] = search_history
+    elif search_history:
+        initial = dict(search=search_text, search_in=search_history[-1][0])
+        form = SearchForm(initial=initial)
     return render(request, "reviews/search-results.html", {"form": form, "search_text": search_text, "books": books})
 
 
@@ -63,6 +77,15 @@ def book_detail(request,pk):
     else:
         context = {"book": book, "book_rating": None, "reviews": None }
 
+    if request.user.is_authenticated:
+        max_viewed_books_length = 10
+        viewed_books = request.session.get('viewed_books', [])
+        viewed_book = [book.id, book.title]
+        if viewed_book in viewed_books:
+            viewed_books.pop(viewed_books.index(viewed_book))
+        viewed_books.insert(0, viewed_book)
+        viewed_books = viewed_books[:max_viewed_books_length]
+        request.session['viewed_books'] = viewed_books
     return render(request, 'reviews/book_details.html', context)
 
 
@@ -82,6 +105,7 @@ def form_example(request):
     return render(request, "reviews/form-example.html", {"method": request.method, "form": form})
 
 
+@user_passes_test(is_staff_user)
 def publisher_edit(request, pk=None):
     if pk is not None:
         publisher = get_object_or_404(Publisher, pk=pk)
@@ -101,10 +125,14 @@ def publisher_edit(request, pk=None):
         return render(request, "reviews/instance-form.html", {"instance": publisher,"model_type": "Publisher", "form": form})
 
 
+@login_required
 def review_edit(request, book_pk, review_pk=None):
     book = get_object_or_404(Book, pk=book_pk)
     if review_pk is not None:
         review = get_object_or_404(Review, book_id=book_pk, pk=review_pk)
+        user = request.user
+        if not user.is_staff and review.creator.id != user.id:
+            raise PermissionDenied
     else:
         review = None
 
@@ -129,6 +157,7 @@ def review_edit(request, book_pk, review_pk=None):
                                                           "related_model_type": Book, "related_instance": book})
 
 
+@login_required
 def book_media(request,pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == "POST":
