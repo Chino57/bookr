2c0a0b6d4ba88dc85e902085093fe43269d6fa6a
Julien Pernin <pernin.julien@gmail.com>
2022-06-10 15:16:24 +0200

    Add test
    Adding files for testing models and views

--- bookr/urls.py
+++ bookr/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from bookr.views import profile
+from .views import profile
 
 urlpatterns = [
     path('filter_demo/', include('filter_demo.urls')),
@@ -25,7 +25,8 @@ urlpatterns = [
     path('accounts/', include(('django.contrib.auth.urls', 'auth'), namespace='accounts')),
     path('accounts/profile/', profile, name='profile'),
     path('admin/', admin.site.urls),
-    path('', include('reviews.urls'))
+    path('', include('reviews.urls')),
+    path('', include('bookr_test.urls'))
 ]
 
 if settings.DEBUG:
---
+++ reviews/tests/test_views.py
@@ -0,0 +1,14 @@
+from django.test import TestCase, Client,RequestFactory
+from ..views import index
+
+
+class TestIndexView(TestCase):
+    """Test the index view"""
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_index_view(self):
+        request = self.factory.get('')
+        request.session = {}
+        response = index(request)
+        self.assertEquals(response.status_code, 200)
